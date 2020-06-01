4f5bc330c353991ceefdda49b89fba7237cbe468
mosaadaldeen <mohammadsd97@gmail.com>
2020-06-01 10:05:54 +0200

    implemented the feedback, changed .then() to async/await

--- homework/index.js
+++ homework/index.js
@@ -86,29 +86,27 @@
         const contributorSection = document.getElementById('contributor');
         const contributorUrl = `https://api.github.com/repos/HackYourFuture/${repo}/contributors`;
         try {
-            await fetch(contributorUrl)
-                .then(data => data.json())
-                .then(response => {
-                    response.forEach(responseInfo => {
-                        const contributor = createAndAppend('a', contributorSection, {
-                            class: 'contributor',
-                            target: '_blank',
-                            href: responseInfo.html_url
-                        });
-                        const img = createAndAppend('img', contributor, {
-                            src: responseInfo.avatar_url,
-                            class: 'image-contributor'
-                        });
-                        const h4 = createAndAppend('h4', contributor, {
-                            text: responseInfo.login,
-                            class: 'name-contributor'
-                        });
-                        const h5 = createAndAppend('h5', contributor, {
-                            text: responseInfo.contributions,
-                            class: 'contributions'
-                        });
-                    });
+            const data = await fetch(contributorUrl);
+            const response = await data.json();
+            response.forEach(responseInfo => {
+                const contributor = createAndAppend('a', contributorSection, {
+                    class: 'contributor',
+                    target: '_blank',
+                    href: responseInfo.html_url
                 });
+                const img = createAndAppend('img', contributor, {
+                    src: responseInfo.avatar_url,
+                    class: 'image-contributor'
+                });
+                const h4 = createAndAppend('h4', contributor, {
+                    text: responseInfo.login,
+                    class: 'name-contributor'
+                });
+                const h5 = createAndAppend('h5', contributor, {
+                    text: responseInfo.contributions,
+                    class: 'contributions'
+                });
+            });
         } catch (error) {
             handleError(error);
         }
@@ -121,27 +119,25 @@
             });
             const repoContainer = document.getElementById('repository');
             const contributorSection = document.getElementById('contributor');
-            await fetch(url)
-                .then(response => response.json())
-                .then(repositories => {
-                    repositories.sort((a, b) => {
-                            return a.name.localeCompare(b.name);
-                        })
-                        .forEach((repo, index) => {
-                            createAndAppend('option', select, {
-                                text: repo.name,
-                                value: index
-                            });
-                        });
-                    select.addEventListener('change', () => {
-                        repoContainer.innerHTML = '';
-                        contributorSection.innerHTML = '';
-                        addInfo(repositories[select.value], repoContainer);
-                        contributorDetail(repositories[select.value].name);
+            const data = await fetch(url);
+            const repositories = await data.json();
+            repositories.sort((a, b) => {
+                    return a.name.localeCompare(b.name);
+                })
+                .forEach((repo, index) => {
+                    createAndAppend('option', select, {
+                        text: repo.name,
+                        value: index
                     });
-                    addInfo(repositories[select.value], repoContainer);
-                    contributorDetail(repositories[select.value].name);
                 });
+            select.addEventListener('change', () => {
+                repoContainer.innerHTML = '';
+                contributorSection.innerHTML = '';
+                addInfo(repositories[select.value], repoContainer);
+                contributorDetail(repositories[select.value].name);
+            });
+            addInfo(repositories[select.value], repoContainer);
+            contributorDetail(repositories[select.value].name);
         } catch (err) {
             handleError(err);
         }
